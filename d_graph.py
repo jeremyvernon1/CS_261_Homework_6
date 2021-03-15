371c7b18e00e52b3a5bab0daf5512773edea13ad
jeremyvernon1 <jeremyvernon@icloud.com>
2021-03-14 20:33:24 -0400

    checkpoint get edges on d_graph

--- d_graph.py
+++ d_graph.py
@@ -6,6 +6,7 @@
 import heapq
 from collections import deque
 
+
 class DirectedGraph:
     """
     Class to implement directed weighted graph
@@ -55,33 +56,77 @@ class DirectedGraph:
 
     def add_vertex(self) -> int:
         """
-        TODO: Write this implementation
+        Adds a vertex to the matrix
         """
-        pass
+        length = self.v_count
+        # increments size
+        self.v_count += 1
+        # adds 0s to the new row
+        self.adj_matrix.append([0] * self.v_count)
+        # adds 0s to the previous rows
+        for index in range(length):
+            self.adj_matrix[(length - 1)].append(0 * (self.v_count - length))
+            length -= 1
+
+        return self.v_count
 
     def add_edge(self, src: int, dst: int, weight=1) -> None:
         """
-        TODO: Write this implementation
+        Adds a weighted edge to the matrix
         """
-        pass
+        # checks if src and dst fall in range
+        # checks if weight is greater than 0
+        # checks that src and dst are not equal
+        if 0 <= src <= self.v_count and \
+                0 <= dst <= self.v_count and \
+                weight > 0 and \
+                src is not dst:
+            # add weight to create edge
+            self.adj_matrix[src][dst] = weight
 
     def remove_edge(self, src: int, dst: int) -> None:
         """
-        TODO: Write this implementation
+        Removes and edge from the graph
         """
-        pass
+        if 0 <= src <= self.v_count and \
+                0 <= dst <= self.v_count and \
+                self.adj_matrix[src][dst] > 0:
+            self.adj_matrix[src][dst] = 0
 
     def get_vertices(self) -> []:
         """
-        TODO: Write this implementation
+        Returns a list of all vertices
         """
-        pass
+        # initializes
+        get_vertices_results = []
+        get_vert_length = len(self.adj_matrix)
+        # searches each cell in the matrix for values greater than 0
+        for row_index in range(get_vert_length):
+            for edge_index in range(get_vert_length):
+                # checks that weight is greater than 0 and vertex not already recorded
+                if self.adj_matrix[row_index][edge_index] > 0 and\
+                        row_index not in get_vertices_results:
+                    # adds vertex to result list
+                    get_vertices_results.append(row_index)
+
+        return get_vertices_results
 
     def get_edges(self) -> []:
         """
-        TODO: Write this implementation
+        Returns a list of tuple edges
         """
-        pass
+        # initializes
+        get_edges_results = []
+        get_vert_length = len(self.adj_matrix)
+        # checks each cell in the matrix for a weight greater than 0
+        for row_index in range(get_vert_length):
+            for edge_index in range(get_vert_length):
+                weight = self.adj_matrix[row_index][edge_index]
+                if weight > 0:
+                    # adds index positions and weight to result list
+                    get_edges_results.append((row_index, edge_index, weight))
+
+        return get_edges_results
 
     def is_valid_path(self, path: []) -> bool:
         """
@@ -130,7 +175,6 @@ if __name__ == '__main__':
         g.add_edge(src, dst, weight)
     print(g)
 
-
     print("\nPDF - method get_edges() example 1")
     print("----------------------------------")
     g = DirectedGraph()
@@ -139,7 +183,6 @@ if __name__ == '__main__':
              (3, 1, 5), (2, 1, 23), (3, 2, 7)]
     g = DirectedGraph(edges)
     print(g.get_edges(), g.get_vertices(), sep='\n')
-
 
     print("\nPDF - method is_valid_path() example 1")
     print("--------------------------------------")
@@ -150,7 +193,6 @@ if __name__ == '__main__':
     for path in test_cases:
         print(path, g.is_valid_path(path))
 
-
     print("\nPDF - method dfs() and bfs() example 1")
     print("--------------------------------------")
     edges = [(0, 1, 10), (4, 0, 12), (1, 4, 15), (4, 3, 3),
@@ -158,7 +200,6 @@ if __name__ == '__main__':
     g = DirectedGraph(edges)
     for start in range(5):
         print(f'{start} DFS:{g.dfs(start)} BFS:{g.bfs(start)}')
-
 
     print("\nPDF - method has_cycle() example 1")
     print("----------------------------------")
@@ -176,7 +217,6 @@ if __name__ == '__main__':
         g.add_edge(src, dst)
         print(g.get_edges(), g.has_cycle(), sep='\n')
     print('\n', g)
-
 
     print("\nPDF - dijkstra() example 1")
     print("--------------------------")
